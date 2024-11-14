874aaee3f690197abe816a4f7ec9aa0862cd09ef
MustafaShf <itsmustafashafique@gmail.com>
2024-11-14 18:48:44 +0500

    minor_chages

--- ElectricApp/program.cpp
+++ ElectricApp/program.cpp
@@ -1,9 +1,12 @@
+
 #include "LoginForm.h"
 #include "Dashboard.h"
 #include "RegisterForm.h"
 #include "inputForm.h"
 
+
 using namespace System;
+
 using namespace System::Windows::Forms;
 
 void main(array<String^>^ args)
@@ -17,50 +20,52 @@ void main(array<String^>^ args)
 
     // Retrieve the authenticated user
 
-    ElectricApp::inputForm input_Form;
-    input_Form.ShowDialog();
+   /* ElectricApp::inputForm input_Form;
+    input_Form.ShowDialog();*/
 
-    //User^ user = nullptr;
-    //while (true)
-    //{
-    //    ElectricApp::LoginForm loginForm;
-    //    loginForm.ShowDialog();
-    //    if (loginForm.switchToRegister)
-    //    {
-    //        ElectricApp::RegisterForm registerForm;
-    //        registerForm.ShowDialog();
-    //        if (registerForm.switchToLogin)
-    //        {
-    //            continue;
-    //        }
-    //        else
-    //        {
-    //            user = registerForm.user;
-    //            break;
-    //        }
-    //    }
-    //    else
-    //    {
-    //        user = loginForm.user;
-    //        break;
-    //    }
-    //   
-    //}
+    
+
+    User^ user = nullptr;
+    while (true)
+    {
+        ElectricApp::LoginForm loginForm;
+        loginForm.ShowDialog();
+        if (loginForm.switchToRegister)
+        {
+            ElectricApp::RegisterForm registerForm;
+            registerForm.ShowDialog();
+            if (registerForm.switchToLogin)
+            {
+                continue;
+            }
+            else
+            {
+                user = registerForm.user;
+                break;
+            }
+        }
+        else
+        {
+            user = loginForm.user;
+            break;
+        }
+       
+    }
 
   
 
-    //if (user != nullptr)
-    //{
-    //  
-    //    ElectricApp::Dashboard dashboardForm;
-    //   /* dashboardForm.StartPosition = FormStartPosition::Manual;
-    //    dashboardForm.Location = loginForm.Location;*/
-    //    Application::Run(% dashboardForm);
-    //}
-    //else
-    //{
-    //    // Authentication canceled
-    //    MessageBox::Show("Authentication Canceled", "Program.cpp", MessageBoxButtons::OK);
-    //    Application::Exit();
-    //}
+    if (user != nullptr)
+    {
+      
+        ElectricApp::Dashboard dashboardForm;
+       /* dashboardForm.StartPosition = FormStartPosition::Manual;
+        dashboardForm.Location = loginForm.Location;*/
+        Application::Run(% dashboardForm);
+    }
+    else
+    {
+        // Authentication canceled
+        MessageBox::Show("Authentication Canceled", "Program.cpp", MessageBoxButtons::OK);
+        Application::Exit();
+    }
 }
